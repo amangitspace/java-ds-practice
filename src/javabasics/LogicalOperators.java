cc42ff7f4aee32efc43bebbe8934289438fcaa6b
amangitspace <aman21march04@gmail.com>
2024-09-28 21:33:55 +0530

    commit 2

--- src/javabasics/LogicalOperators.java
+++ src/javabasics/LogicalOperators.java
@@ -4,6 +4,7 @@ public class LogicalOperators {
         int a =10;
         int b =10;
         int c =40;
+        int d =100;
 
         System.out.println(a==b && a==c);
         System.out.println(a==b || a==c);
